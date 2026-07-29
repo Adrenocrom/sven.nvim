2b4612ffcfea7d3175ee2c5b7a1119c8a052edaf
Josef Schulz <j.schulz@alaun.de>
2026-07-29 11:21:13 +0200

    U

--- lua/sven/window.lua
+++ lua/sven/window.lua
@@ -139,6 +139,18 @@ local function open_markdown_chat(cmd, prepared_prompt, make_win, config)
 			if not data then
 				return
 			end
+			for i = 1, #data - 1 do
+				append(data[i] .. '\n')
+			end
+			local last = data[#data]
+			if last and last ~= '' then
+				append(last)
+			end
+			flush()
+		end,
+			if not data then
+				return
+			end
 			for i = 1, #data do
 				append(data[i])
 			end
