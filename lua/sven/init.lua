016cf2e3917cb90322bb9546083feb85a0913fce
Josef Schulz <j.schulz@alaun.de>
2026-07-30 07:54:06 +0200

    U

--- lua/sven/init.lua
+++ lua/sven/init.lua
@@ -19,15 +19,16 @@ function M.setup(user_config)
 	vim.api.nvim_create_user_command('Sven', function(opts)
 		local display_mode = opts.args ~= '' and opts.args or M.config.default_window
 
-		if arg.range == 0 then
-			mode = "n"
+		local range_type
+		if opts.range == 0 then
+			range_type = "n"
 		else
-			mode = "v"
+			range_type = "v"
 		end
 		local list = {"ask", "chat"}
 		vim.ui.select(list, {prompt = "Choose:" }, function(item) 
 			if item == "ask" then
-				M.ask(display_mode, mode)
+				M.ask(display_mode, range_type)
 			elseif item == "chat" then
 				M.open(display_mode)
 			end
@@ -82,23 +83,23 @@ function M.open(display_mode)
 	end
 end
 
-function M.ask(display_mode, mode, user_prompt)
+function M.ask(display_mode, range_type, user_prompt)
 	display_mode = display_mode or M.config.default_window
 	local bufnr = vim.api.nvim_get_current_buf()
 
 	if user_prompt then
-		M.open_with_prompt(display_mode, bufnr, user_prompt)
+		M.open_with_prompt(display_mode, range_type, bufnr, user_prompt)
 		return
 	end
 
 	vim.ui.input({ prompt = 'Ask sven: ' }, function(input)
-		M.open_with_prompt(display_mode, bufnr, input or '')
+		M.open_with_prompt(display_mode, range_type, bufnr, input or '')
 	end)
 end
 
-function M.open_with_prompt(display_mode, bufnr, user_prompt)
+function M.open_with_prompt(display_mode, range_type, bufnr, user_prompt)
 	vim.schedule(function()
-		local prepared = prompt.build(bufnr, user_prompt, M.config.prompt_template)
+		local prepared = prompt.build(bufnr, user_prompt, M.config.prompt_template, range_type)
 
 		if display_mode == 'float' then
 			window.open_float(M.config.float, prepared, M.config)
